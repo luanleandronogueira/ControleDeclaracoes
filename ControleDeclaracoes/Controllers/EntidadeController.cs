d9af9a822ee2227cbc7dfcc93d1b5c04a465a1a7
Luan Leandro Nogueira <108754022+luanleandronogueira@users.noreply.github.com>
2024-08-30 15:35:01 -0300

    Add files via upload

---
+++ ControleDeclaracoes/Controllers/EntidadeController.cs
@@ -0,0 +1,45 @@
+﻿using ControleDeclaracoes.Models;
+using ControleDeclaracoes.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeclaracoes.Controllers
+{
+    public class EntidadeController : Controller
+    {
+        private readonly IEntidadeRepositorio _entidadeRepositorio;
+
+        public EntidadeController(IEntidadeRepositorio entidadeRepositorio)
+        {
+            _entidadeRepositorio = entidadeRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult cadastraEntidade(EntidadeModel entidade)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(entidade.Detalhes))
+                {
+                    entidade.Detalhes = "Sem Detalhes";
+                }
+
+                _entidadeRepositorio.cadastraEntidade(entidade);
+                TempData["MensagemSucesso"] = "Cadastro Feito com Sucesso";
+
+            }
+            catch (SystemException erro)
+            {
+                TempData["MensagemErro"] = $"Erro ao Cadastrar o contato, erro: {erro.Message}";
+            }
+
+            // Redireciona após definir o TempData
+            return RedirectToAction("Index");
+        }
+
+    }
+}
